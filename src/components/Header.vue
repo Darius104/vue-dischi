e64dd5727fd9976c3fc3a53c469ae1810d695d4a
Darius104 <comandarius@gmail.com>
2021-12-27 13:28:18 +0100

    Header Done

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/spotify.png" alt="">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+    header{
+        height: 70px;
+        width: 100%;
+        background-color: $color_header;
+        display: flex;
+        align-items: center;
+
+        .logo{
+            width: 100px;
+        }
+    }
+</style> 
